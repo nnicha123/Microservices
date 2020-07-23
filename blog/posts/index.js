89f5f0fc5df90382892ba11c9751ad874b07e8f7
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-07-23 10:58:26 +0700

    finish blog react app -> but now need to find microservice solution

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const {randomBytes} = require('crypto')
+const cors = require('cors')
 
 const app = express()
 app.use(bodyParser.json())
+app.use(cors())
+
 const posts = {}
 
 app.get('/posts',(req,res) => {
@@ -12,7 +15,7 @@ app.get('/posts',(req,res) => {
 app.post('/posts',(req,res) => {
     const id = randomBytes(4).toString('hex')
     const {title} = req.body
-    post[id] = {
+    posts[id] = {
         id,title
     }
     res.status(201).send(posts[id])
