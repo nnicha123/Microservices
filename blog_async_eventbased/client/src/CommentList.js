16d978e8756e9cf4cc47f2913d84a4d844124cdb
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-07-23 14:27:51 +0700

    store changes when query is down and get all events it has missed when restart

--- blog_async_eventbased/client/src/CommentList.js
+++ blog_async_eventbased/client/src/CommentList.js
@@ -1,18 +1,19 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React from 'react'
 
-export default ({ postId }) => {
+export default ({ comments }) => {
 
-    const [comments, setComments] = useState([])
-    const fetchData = async () => {
-        const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`);
-        setComments(res.data)
-    }
-    useEffect(() => {
-        fetchData()
-    }, [])
     const renderedComments = comments.map(comment => {
-        return <li key={comment.id}>{comment.content}</li>
+        let content
+        if(comment.status ==='approved'){
+            content = comment.content
+        }
+        if(comment.status === 'pending'){
+            content = 'This comment is awaiting moderation'
+        }
+        if(comment.status === 'rejected'){
+            content = 'This comment has been rejected'
+        }
+        return <li key={comment.id}>{content}</li>
     })
 
 return <ul>{renderedComments}</ul>
