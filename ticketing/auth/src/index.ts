6b54ea5d36827888d583ec950e491d8c034d58cb
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-07-29 08:39:01 +0700

    added ingress file and changed host file to run file

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -3,6 +3,11 @@ import { json } from 'body-parser';
 
 const app = express();
 app.use(json());
+
+app.get('/api/users/currentuser', (req, res) => {
+  res.send('HELLOWWWW there!');
+});
+
 app.listen(3000, () => {
   console.log('Listening on port 3000!!!!!!');
 });
