0593514d479fa05f9f525f12c04be4f238050339
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-07-29 14:23:51 +0700

    added authentication using session and received a cookie

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response, response } from 'express';
 import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
 import { User } from '../models/User';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { BadRequestError } from '../errors/bad-request-error';
@@ -30,6 +32,19 @@ router.post(
     }
     const user = User.build({ email, password });
     await user.save();
+    // Generate JWT
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      'earg'
+    );
+
+    // Store it on session object
+    req.session = {
+      jwt: userJwt,
+    };
     res.status(201).send(user);
   }
 );
